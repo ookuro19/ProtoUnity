2e4862626e6305b1cce2f351ba183ed3e2674e9e
NeunZehn <19asahi@gmail.com>
2023-04-11 23:25:38 +0800

    generate all of proto in target folder

--- Assets/Framework/Editor/PBGenerator.cs
+++ Assets/Framework/Editor/PBGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.Diagnostics;
+using System.IO;
 using Debug = UnityEngine.Debug;
 
 namespace Framework
@@ -9,19 +10,36 @@ namespace Framework
     {
         // Example code for using protoc in Unity
         // Assumes protoc.exe is in the project's root directory
-
-        static string protocPath = Application.dataPath + "/../Tools/protoc.exe";
-        static string protoFolderPath = Application.dataPath + "/../ProtoMessage";
-        static string protoFilePath = $"chat.proto";
-        static string outputDir = Application.dataPath + "/Scripts/ProtoCS";
+        // todo: serialize in project setting
+        static string protocPath = Application.dataPath + "/../Tools/protoc-22.2-win64/bin/protoc.exe";
+        static string protoFolderPath = Application.dataPath + "/Scripts/GenerateCode";
+        static string outputDir = Application.dataPath + "/Scripts/GenerateCode";
 
 
         [MenuItem("Proto/Generate CS")]
-        public static void Proto2PB()
+        public static void GenerateCSFromProto()
+        {
+            if (!Directory.Exists(protoFolderPath))
+            {
+                Debug.Log($"指定目录不存在: {protoFolderPath}");
+                return;
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(protoFolderPath);
+            var fileInfos = directoryInfo.GetFiles();
+            foreach (var fileInfo in fileInfos)
+            {
+                if (fileInfo.Extension == ".proto")
+                {
+                    Proto2PB(fileInfo);
+                }
+            }
+        }
+
+        public static void Proto2PB(FileInfo fileInfo)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = protocPath;
-            startInfo.Arguments = $"--csharp_out={outputDir} --proto_path={protoFolderPath} {protoFilePath}";
+            startInfo.Arguments = $"--csharp_out={outputDir} --proto_path={protoFolderPath} {fileInfo.FullName}";
             startInfo.RedirectStandardOutput = true;
             startInfo.RedirectStandardError = true;
             startInfo.UseShellExecute = false;
